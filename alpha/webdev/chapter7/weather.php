a814a6f4e50784e71d2f719fcb0a3b819998a779
Jakobtottrup <jakobtottrup@gmail.com>
2015-07-18 17:04:28 +0200

    finished PHP

--- alpha/webdev/chapter7/weather.php
+++ alpha/webdev/chapter7/weather.php
@@ -1,93 +1,109 @@
-<!DOCTYPE html>
-<html lang="en">
+<!doctype html>
+<html>
 <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <title>Weather-scraper</title>
-
-    <!-- Bootstrap -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <title>Weather Scraper</title>
+    <meta charset="utf-8"/>
+    <meta http-equiv="Content-type" content="text/html; charset=utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
     <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-
+    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/
+bootstrap.min.css">
     <!-- Optional theme -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
-
-    <!-- Latest compiled and minified JavaScript -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-    <![endif]-->
+    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/
+bootstrap-theme.min.css">
     <style>
-        html,body {
+        html, body {
             height: 100%;
         }
+
         .container {
-            background-image: url(../../images/weather.jpg);
+            background-image: url("../../images/weather.jpg");
             width: 100%;
             height: 100%;
             background-size: cover;
             background-position: center;
-            padding-top: 150px;
+            padding-top: 100px;
         }
-        .white {
-            color: white;
-        }
+
         .center {
             text-align: center;
         }
+
+        .white {
+            color: white;
+        }
+
         p {
             padding-top: 15px;
             padding-bottom: 15px;
         }
-        input {
 
+        button {
+            margin-top: 20px;
+            margin-bottom: 20px;
         }
 
-        #submitBtn {
+        .alert {
             margin-top: 20px;
+            display: none;
+            /*position: relative;*/
         }
     </style>
 </head>
 <body>
 <div class="container">
     <div class="row">
-        <div class="col-md-6 col-md-offset-3 center white">
-            <h1>Weather scraper project.</h1>
-            <p class="lead">Enter your city below to recieve weather forecasts</p>
+        <div class="col-md-6 col-md-offset-3 center">
+            <h1 class="center white">Weather Predictor</h1>
+            <p class="lead center white">Enter your city below to get a 3-day forecast for the weather.</p>
 
-            <form action="">
-               <div class="form-group">
-                   <input type="text" class="form-control" placeholder="Enter your city..." name="city" id="city">
-
-               </div>
-                <input type="submit" class="btn btn-success btn-lg" id="submitBtn">
-
-
+            <form>
+                <div class="form-group">
+                    <input type="text" class="form-control" name="city" id="city" placeholder="Eg.London, Paris, San Francisco..."/>
+                </div>
+                <button id="findMyWeather" class="btn btn-success btn-lg">Find My Weather</button>
             </form>
+
+            <div id="success" class="alert alert-success">
+                Success!
+            </div>
+
+            <div id="fail" class="alert alert-danger">
+                <p>Could not find weather data for that city. Please try again.</p>
+            </div>
+
+            <div id="noCity" class="alert alert-danger">
+                Please enter a city!
+            </div>
+
         </div>
 
     </div>
-
 </div>
+<script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+<!-- Latest compiled and minified JavaScript -->
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+<script>
+    $("#findMyWeather").click(function (event) {
+        event.preventDefault();
+        $(".alert").hide();
 
-<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-<!-- Include all compiled plugins (below), or include individual files as needed -->
-<script src="js/bootstrap.min.js"></script>
+        if ($("#city").val() != "") {
+
+            $.get("scraper.php?city=" + $("#city").val(), function (data) {
+
+
+                if (data == "") {
+                    $("#fail").fadeIn();
+
+                } else {
+                    $("#success").html(data).fadeIn();
+                }
+            });
+        } else {
+            $("#noCity").fadeIn();
+        }
+    });
+</script>
 </body>
 </html>
-
-<?php
-/**
- * Created by PhpStorm.
- * User: Jakob
- * Date: 13-07-2015
- * Time: 17:34
- */
